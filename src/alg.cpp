c3c6c23abe7fd7fb7775e4c674927c6f3051a4c2
Stanislav Zelenkov <124909037+MaldorianR200@users.noreply.github.com>
2023-02-26 21:18:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,51 @@
 
 
 double pown(double value, uint16_t n) {
-
+  double result = 1;
+  for (int i = 0; i < n; i++){
+    result *= value;
+  }
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-
+  if (n == 0 || n == 1){
+    return 1;
+  } else {
+    return n * fact(n - 1);
+  }
 }
 
 double calcItem(double x, uint16_t n) {
-
+  double sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += (pown(x, n) / fact(n));
+  }
+  return sum;
 }
 
 double expn(double x, uint16_t count) {
-
+  double ex = 1, p = 1, f = 1;
+  for (int i = 0; i < count; i++) {
+    ex += (pown(x, p) / fact(f));
+    p++;
+    f++;
+  }
+  return ex;
 }
 
 double sinn(double x, uint16_t count) {
-
+  double result = x, n = 1;
+  for (int i = 0, i < count; i++) {
+    result += pown(-1, n - 1) * (pown(x, 2 * n - 1) / fact(2 * n - 2));
+  }
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-
+  double result = 1, n = 1;
+  for (int i = 0; i < count; i++) {
+    result += pown(-1, n - 1) * (pown(x, 2 * n - 1) / fact(2 * n - 2));
+  }
+  return result;
 }
